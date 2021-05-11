2db205b1b242aedbe97997b185d00a4947c414f7
gclough <gareth@electricocean.eu>
2021-05-11 11:49:23 +0100

    FWV

--- app/src/main/java/eu/electricocean/quiz/QuizQuestionsActivity.kt
+++ app/src/main/java/eu/electricocean/quiz/QuizQuestionsActivity.kt
@@ -1,11 +1,17 @@
 package eu.electricocean.quiz
 
+import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import eu.electricocean.quiz.databinding.ActivityQuizQuestionsBinding
 
-class QuizQuestionsActivity : AppCompatActivity() {
+class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var binding: ActivityQuizQuestionsBinding
     private var mCurrentPosition:Int = 1
     private var mQuestionsList: ArrayList<Question>? = null
@@ -17,12 +23,16 @@ class QuizQuestionsActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         mQuestionsList = Constants.getQuestions()
-        Log.i("Questions Size","${questionsList.size}")
         setQuestion()
+        binding.tvOptionOne.setOnClickListener(this)
+        binding.tvOptionTwo.setOnClickListener(this)
+        binding.tvOptionThree.setOnClickListener(this)
+        binding.tvOptionFour.setOnClickListener(this)
+        binding.btnSubmit.setOnClickListener(this)
     }
 
     private fun setQuestion() {
-        val question: Question? = mQuestionsList[mCurrentPosition - 1]
+        val question: Question? = mQuestionsList?.get(mCurrentPosition - 1)
         binding.progressBar.progress = mCurrentPosition
         binding.tvProgress.text = "$mCurrentPosition" + "/" + binding.progressBar.max
         binding.tvQuestion.text = question!!.question
@@ -31,6 +41,12 @@ class QuizQuestionsActivity : AppCompatActivity() {
         binding.tvOptionTwo.text = question.optionTwo
         binding.tvOptionThree.text = question.optionThree
         binding.tvOptionFour.text = question.optionFour
+        defaultOptionsView()
+        if (mCurrentPosition == mQuestionsList!!.size) {
+            binding.btnSubmit.text = "FINISH"
+        } else {
+            binding.btnSubmit.text = "SUBMIT"
+        }
     }
 
     private fun defaultOptionsView() {
@@ -39,5 +55,88 @@ class QuizQuestionsActivity : AppCompatActivity() {
         options.add(1,binding.tvOptionTwo)
         options.add(2,binding.tvOptionThree)
         options.add(3,binding.tvOptionFour)
+
+        for (option in options) {
+            option.setTextColor(Color.parseColor("#7A8089"))
+            option.typeface = Typeface.DEFAULT
+            option.background = ContextCompat.getDrawable(
+                this,
+                R.drawable.default_option_border_bg
+            )
+        }
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id) {
+            R.id.tv_option_one ->{
+                selectedOptionView(binding.tvOptionOne,1)
+            }
+            R.id.tv_option_two ->{
+                selectedOptionView(binding.tvOptionTwo,2)
+            }
+            R.id.tv_option_three ->{
+                selectedOptionView(binding.tvOptionThree,4)
+            }
+            R.id.tv_option_four ->{
+                selectedOptionView(binding.tvOptionFour,5)
+            }
+            R.id.btn_submit ->{
+                if (mSelectedOptionPosition == 0) {
+                    mCurrentPosition++
+                    when {
+                        mCurrentPosition <= mQuestionsList!!.size -> {
+                            setQuestion();
+                        }
+                        else -> {
+                            Toast.makeText(
+                                this,
+                                "You have successfully completed the quiz",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+                } else {
+                    val question = mQuestionsList?.get(mCurrentPosition - 1)
+                    if (question!!.correctAnswer != mSelectedOptionPosition) {
+                        answerView(mSelectedOptionPosition,R.drawable.wrong_option_border_bg)
+                    }
+                    answerView(question.correctAnswer,R.drawable.correct_option_border_bg)
+                    if(mCurrentPosition == mQuestionsList!!.size) {
+                        binding.btnSubmit.text = "FINISH"
+                    } else {
+                        binding.btnSubmit.text = "GO TO NEXT QUESTION"
+                    }
+                    mSelectedOptionPosition = 0
+                }
+            }
+        }
+    }
+
+    private fun answerView(answer: Int,drawableView: Int) {
+        when(answer) {
+            1 ->{
+                binding.tvOptionOne.background = ContextCompat.getDrawable(this,drawableView)
+            }
+            2 ->{
+                binding.tvOptionTwo.background = ContextCompat.getDrawable(this,drawableView)
+            }
+            3 ->{
+                binding.tvOptionThree.background = ContextCompat.getDrawable(this,drawableView)
+            }
+            4 ->{
+                binding.tvOptionFour.background = ContextCompat.getDrawable(this,drawableView)
+            }
+        }
+    }
+
+    private fun selectedOptionView(tv: TextView, selectedOptionNum: Int) {
+        defaultOptionsView()
+        mSelectedOptionPosition = selectedOptionNum
+        tv.setTextColor(Color.parseColor("#363A43"))
+        tv.typeface = Typeface.DEFAULT_BOLD
+        tv.background = ContextCompat.getDrawable(
+            this,
+            R.drawable.default_option_border_bg
+        )
     }
 }
