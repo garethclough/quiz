ed0a15fcf86f961810727be8404495a401102249
gclough <gareth@electricocean.eu>
2021-05-23 14:36:14 +0100

    Added volley

--- app/src/main/java/eu/electricocean/quiz/Constants.kt
+++ app/src/main/java/eu/electricocean/quiz/Constants.kt
@@ -4,6 +4,7 @@ object Constants {
     const val USER_NAME: String = "user_name"
     const val TOTAL_QUESTIONS: String = "total_question"
     const val CORRECT_ANSWERS: String = "correct_answers"
+    const val LOGTAG: String = "quizlord"
 
     fun getQuestions(): ArrayList<Question> {
         var questionsList = ArrayList<Question>()
--- app/src/main/java/eu/electricocean/quiz/MainActivity.kt
+++ app/src/main/java/eu/electricocean/quiz/MainActivity.kt
@@ -3,16 +3,54 @@ package eu.electricocean.quiz
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import com.android.volley.Response
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import eu.electricocean.quiz.databinding.ActivityMainBinding
+import org.json.JSONException
+import org.json.JSONObject
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Instantiate the RequestQueue.
+        val queue = Volley.newRequestQueue(this)
+        val url = "http://quiz.electricocean.eu/get"
+
+        // Request a string response from the provided URL.
+        val request = JsonArrayRequest(
+            Request.Method.GET,
+            url,
+            null,
+            { response ->
+                try {
+                    for (i in 0 until response.length()) {
+                        val question: JSONObject = response.getJSONObject(i)
+                        val questionString: String = question.getString("question")
+                        Log.d(Constants.LOGTAG,questionString)
+                    }
+                } catch (e: JSONException) {
+                    Log.d(Constants.LOGTAG,e.message!!)
+                }
+
+            },
+            Response.ErrorListener {
+                Log.d(Constants.LOGTAG, "request error")
+            }
+        )
+
+// Add the request to the RequestQueue.
+        queue.add(request)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
